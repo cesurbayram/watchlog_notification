eade3193107f77e247a42067dc63af5c72372c00
Cesur Bayram <cesr.bayram@gmail.com>
2026-01-20 14:31:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Server } from "socket.io";
+import { createServer } from "http";
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.NOTIFICATION_SOCKET_PORT || 4001;
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} user connected!`);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected!");
+  });
+});
+
+const broadcastNotification = (notification) => {
+  io.emit("new_notification", notification);
+};
+
+app.use(express.json());
+app.post("/notify", (req, res) => {
+  const notification = req.body;
+  console.log("notification", notification);
+  broadcastNotification(notification);
+  res.json({ success: true });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
